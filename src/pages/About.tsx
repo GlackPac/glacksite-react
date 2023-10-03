f54419aa8ef6f135bc52ff9f8b05a0a296c070f3
GlackPac <mylesglacken@gmail.com>
2023-10-03 09:25:48 +0800

    npm audit

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,7 +4,7 @@ import { Container } from 'react-bootstrap';
 const About = (props:any) => {
     return (
         <Container fluid>
-            <p>Hello, i'm Home</p>
+            <p>Hello, i'm About</p>
         </Container>
     )
 }
