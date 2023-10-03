4ab74b8dfa3d3f6e278a4c7b318ec8405cd4fffe
GlackPac <mylesglacken@gmail.com>
2023-10-03 09:33:01 +0800

    Updates

---
+++ precache-manifest.af959f9f5badda0aaaef8f1f534ee4ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "79b3d90bc147fc22e90b",
+    "url": "/glacksite/static/css/main.b85ca34f.chunk.css"
+  },
+  {
+    "revision": "79b3d90bc147fc22e90b",
+    "url": "/glacksite/static/js/main.79b3d90b.chunk.js"
+  },
+  {
+    "revision": "91e5a6e9c0e452399fa0",
+    "url": "/glacksite/static/css/1.0c529c44.chunk.css"
+  },
+  {
+    "revision": "91e5a6e9c0e452399fa0",
+    "url": "/glacksite/static/js/1.91e5a6e9.chunk.js"
+  },
+  {
+    "revision": "8aca1ec294a7a84aeacf",
+    "url": "/glacksite/static/js/runtime~main.8aca1ec2.js"
+  },
+  {
+    "revision": "5d2cddde5f2bcbe65d84e68790bf176c",
+    "url": "/glacksite/static/media/raven.5d2cddde.svg"
+  },
+  {
+    "revision": "71dbe4fd10efad62361942afcb15859e",
+    "url": "/glacksite/index.html"
+  }
+];
